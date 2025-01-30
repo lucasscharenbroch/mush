b075dd660b4c281e90fabcd3f3493c4ff534eb85
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2025-01-29 19:52:48 -0600

    Add dir argument to `mush init`

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -1,8 +1,12 @@
+use std::path::Path;
+
 use crate::cli::{ExitType, MushSubcommand};
-use crate::io::{create_directories_no_overwrite, create_file_no_overwrite};
+use crate::io::{create_directories_no_overwrite, create_directory_all, create_file_no_overwrite};
 
 #[derive(clap::Args)]
-pub struct InitArgs {}
+pub struct InitArgs {
+    directory: Option<String>,
+}
 
 impl MushSubcommand for InitArgs {
     fn execute(&self) -> ExitType {
@@ -10,13 +14,30 @@ impl MushSubcommand for InitArgs {
         const DEFAULT_CONFIG: &'static str = "[[default-config-placeholder]]";
         const DEFAULT_HEAD: &'static str = "[[default-head-placeholder]]";
 
+        let directory = match self.directory {
+            Some(ref dir) => {
+                crate::cli_expect!(create_directory_all(dir), "create directory for repository");
+                let path = Path::new(&dir).canonicalize().expect(&format!("Failed to canonicalize path: `{dir}`"));
+                String::from(path.to_str().expect("Couldn't stringify path: `{path}`"))
+            },
+            None => {
+                String::from(".")
+            },
+        };
+
         crate::cli_expect!(
-            create_directories_no_overwrite(["./.mush", "./.mush/objects", "./.mush/refs"].iter()),
+            create_directories_no_overwrite(
+                [
+                    format!("{directory}/.mush").as_str(),
+                    format!("{directory}/.mush/objects").as_str(),
+                    format!("{directory}/.mush/refs").as_str(),
+                ].iter()
+            ),
             REASON
         );
 
-        crate::cli_expect!(create_file_no_overwrite("./.mush/config", DEFAULT_CONFIG.as_bytes()), REASON);
-        crate::cli_expect!(create_file_no_overwrite("./.mush/HEAD", DEFAULT_HEAD.as_bytes()), REASON);
+        crate::cli_expect!(create_file_no_overwrite(&format!("{directory}/.mush/config"), DEFAULT_CONFIG.as_bytes()), REASON);
+        crate::cli_expect!(create_file_no_overwrite(&format!("{directory}/.mush/HEAD"), DEFAULT_HEAD.as_bytes()), REASON);
 
         ExitType::Ok
     }
--- tests/mush_init.rs
+++ tests/mush_init.rs
@@ -44,3 +44,55 @@ fn cwd_init() {
 
     assert_dir_is_proper_clean_repo(dir.path());
 }
+
+#[test]
+fn various_dirs_init() {
+    const INIT_DIRS: &[&'static str] = &[
+        "./",
+        "./a",
+        "./abc",
+        "./abc-def.g",
+        "a",
+        "abc",
+        "abc-def.g",
+    ];
+
+    INIT_DIRS.iter().for_each(|init_dir| {
+        let dir = tempdir();
+
+        assert!(
+            mush!(dir)
+                .arg("init")
+                .arg(init_dir)
+                .status()
+                .unwrap()
+                .success()
+        );
+
+        assert_dir_is_proper_clean_repo(&dir.path().join(init_dir));
+    });
+}
+
+#[test]
+fn various_nested_dirs_init() {
+    const INIT_DIRS: &[&'static str] = &[
+        "./a/b/c",
+        "./a/b/c/d/e/f",
+        "./ab/cd/e/f",
+    ];
+
+    INIT_DIRS.iter().for_each(|init_dir| {
+        let dir = tempdir();
+
+        assert!(
+            mush!(dir)
+                .arg("init")
+                .arg(init_dir)
+                .status()
+                .unwrap()
+                .success()
+        );
+
+        assert_dir_is_proper_clean_repo(&dir.path().join(init_dir));
+    });
+}
